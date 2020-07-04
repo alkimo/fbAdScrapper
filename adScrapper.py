25c59e950b00e46a8bda859293e41bace3219a0d
alkimo <alkimo@protonmail.com>
2020-07-04 19:23:13 -0300

    Cleaned the code

--- adScrapper.py
+++ adScrapper.py
@@ -1,87 +1,98 @@
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 from bs4 import BeautifulSoup as bs
-from modules import parser
+import modules.parser
 import os
 import time
 from PIL import Image
 import io
 import time
 
-###FIREFOX driver
-# driverPath = os.getcwd() + '/driver/Gecko/geckodriver'
-# driverType = "firefox"
-# driver = webdriver.Firefox(executable_path=driverPath)
 
-###CHROME driver
-
-driverPath = os.getcwd() + '/driver/Chromedriver/chromedriver'
-driverType = "chrome"
-driver = webdriver.Chrome(executable_path=driverPath)
-
-## Create Dir
-myUrl = parser.initParse()
-timestr = time.strftime("%Y%m%d-%H%M%S")
-filePath = os.getcwd() + '/searchResult/ ' + driverType + ' - ' + timestr
-os.mkdir(filePath)
-driver.get(myUrl.url)
-
-## Saving Pictures code
-
-time.sleep(1)
-
-images = driver.find_elements_by_class_name('_99s5')
-iterator = 1
-
-for image in images:
-    image = image.screenshot_as_png
-    imageStream = io.BytesIO(image)
-    im = Image.open(imageStream)
-    im.save(filePath + '/ ' + str(iterator) + '.png')
-    iterator += 1
-
-last_height = driver.execute_script("return document.body.scrollHeight")
-
-while(True):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-
-    if new_height == last_height:
-        break
-
-    last_height = new_height
+def select_driver(browser):
+    if browser == "Chrome" or browser == "chrome":
+        driver_path = os.getcwd() + '/driver/Chromedriver/chromedriver'
+        return webdriver.Chrome(executable_path=driver_path)
+    elif browser == "Firefox" or browser == "firefox":
+        driver_path = os.getcwd() + '/driver/Gecko/geckodriver'
+        return webdriver.Firefox(executable_path=driver_path)
 
 
-## HTML saving
-html = driver.execute_script("return document.body.innerHTML;")
-soup = bs(html, 'html.parser')
+def save_ad_pictures(driver, folder_path):
+    scroll_to_page_floor(driver)
+    images = driver.find_elements_by_class_name('_99s5')
+    iterator = 1
+    for image in images:
+        image = image.screenshot_as_png
+        imageStream = io.BytesIO(image)
+        im = Image.open(imageStream)
+        im.save(folder_path + '/ ' + str(iterator) + '.png')
+        iterator += 1
 
-driver.close()
-ads = soup.find_all("div", class_="_8n-_")
 
-with open('template.html', 'r') as f: 
+def create_ads_folder(browser, name):
+    folder_path = os.getcwd() + '/searchResult/ ' + browser + ' - ' + name
+    os.mkdir(folder_path)
+    return folder_path
+
+
+def scroll_to_page_floor(driver):
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while(True):
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+
+def save_html(driver, ads_folder_path):
+    html = driver.execute_script("return document.body.innerHTML;")
+    soup = bs(html, 'html.parser')
+
+    driver.close()
+    ads = soup.find_all("div", class_="_8n-_")
+
+    with open('template.html', 'r') as f:
         template = f.read()
         template_soup = bs(template, 'html.parser')
 
-for ad in ads:
-    main_div = template_soup.find("div", class_="_99s8")
-    main_div.insert(1, ad)
+    for ad in ads:
+        main_div = template_soup.find("div", class_="_99s8")
+        main_div.insert(1, ad)
 
-floating_bar = template_soup.find(id="secondaryFiltersV2")
-floating_bar.decompose()
+    floating_bar = template_soup.find(id="secondaryFiltersV2")
+    floating_bar.decompose()
 
-buttons_div = template_soup.find("div", class_="_8ml1")
-buttons_div.decompose()
+    buttons_div = template_soup.find("div", class_="_8ml1")
+    buttons_div.decompose()
 
-search_bar = template_soup.find("div", class_="_8kew")
-search_bar.decompose()
+    search_bar = template_soup.find("div", class_="_8kew")
+    search_bar.decompose()
 
-selects = template_soup.find_all("div", class_="_7kfi")
+    selects = template_soup.find_all("div", class_="_7kfi")
 
-for match in selects:
-    match.decompose()
+    for match in selects:
+        match.decompose()
 
-Html_file = open(filePath + "/search.html","w")
-Html_file.write(str(template_soup))
-Html_file.close()
+    Html_file = open(ads_folder_path + "/ads.html", "w")
+    Html_file.write(str(template_soup))
+    Html_file.close()
+
+
+def main():
+    cli_input = modules.parser.initParse()
+    driver = select_driver(cli_input.b)
+
+    ads_folder_name = cli_input.n if cli_input.n else time.strftime("%Y%m%d-%H%M%S")
+
+    ads_folder_path = create_ads_folder(cli_input.b, ads_folder_name)
+    driver.get(cli_input.url)
+    save_ad_pictures(driver, ads_folder_path)
+    save_html(driver, ads_folder_path)
+
+
+if __name__ == "__main__":
+    main()
--- modules/parser.py
+++ modules/parser.py
@@ -4,13 +4,22 @@ def initParse():
     # Initiate the parser
     parser = argparse.ArgumentParser()
 
-    # Add long and short argument
-    parser.add_argument("-url", help="Primeiro valor de pesquisa")
+    # Link da pesquisa do facebook ads
+    parser.add_argument("-url", help="Url do ad do facebook")
+
+    # Nome do browser a ser usado
+    parser.add_argument("-b", help="Primeiro valor de pesquisa")
+
+    # Nome da pasta para salvar os dados
+    parser.add_argument("-n", help="Nome da pasta a salvar os dados")
 
     # Read arguments from the command line
     args = parser.parse_args()
 
     if(args.url == None):
-        args.url = "https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=ALL&impression_search_field=has_impressions_lifetime&view_all_page_id=59685491632&sort_data[direction]=desc&sort_data[mode]=relevancy_monthly_grouped"
+        args.url = "https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=BR&impression_search_field=has_impressions_lifetime&view_all_page_id=164299666946033&sort_data[direction]=desc&sort_data[mode]=relevancy_monthly_grouped"
+
+    if(args.b == None):
+        args.b = "Chrome"
 
     return args
