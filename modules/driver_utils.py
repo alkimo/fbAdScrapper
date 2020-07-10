c4c58e91b2f4bbb0eb766a5864024ec77b8877ac
alkimo <alkimo@protonmail.com>
2020-07-10 14:58:49 -0300

    Changed the os.getcwd() used in driver_utils for the /home/ +  getpass.getuser()

--- modules/driver_utils.py
+++ modules/driver_utils.py
@@ -2,18 +2,19 @@ from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 import os
 import pdfkit 
+import getpass
 
 
 def select_driver(browser):
     if browser == "Chrome" or browser == "chrome":
-        driver_path = os.getcwd() + '/Desktop/fbScrapper/driver/Chromedriver/chromedriver'
+        driver_path = '/home/' + getpass.getuser() +  '/Desktop/fbScrapper/driver/Chromedriver/chromedriver'
         chrome_options = webdriver.chrome.options.Options()
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         return webdriver.Chrome(executable_path=driver_path, chrome_options=chrome_options)
     elif browser == "Firefox" or browser == "firefox":
-        driver_path = os.getcwd() + '/Desktop/fbScrapper/driver/Gecko/geckodriver'
+        driver_path = '/home/' + getpass.getuser() + '/Desktop/fbScrapper/driver/Gecko/geckodriver'
         return webdriver.Firefox(executable_path=driver_path)
 
 
